188e002b9cc5aef57e90598705ec3feb23eb94a1
Patrick Laxton <patrick.laxton@gmail.com>
2013-11-06 18:32:28 +0100

    chapitre 13 - géoloc - pages 540 à 542

--- src/com/example/whereami/WhereAmI.java
+++ src/com/example/whereami/WhereAmI.java
@@ -1,15 +1,39 @@
 package com.example.whereami;
 
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class WhereAmI extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_where_am_i);
+		setContentView(R.layout.main);
+		
+		LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		
+		Location l = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		
+		updateWithNewLocation(l);
+	}
+	
+	private void updateWithNewLocation (Location location) {
+		TextView myLocationText = (TextView)findViewById(R.id.myLocationText);
+		
+		String latLongString = "You seem to be nowhere.";
+		if (null != location) {
+			double lat = location.getLatitude();
+			double lon = location.getLongitude();
+			
+			latLongString = "Lat : " + lat + "\nLong : " + lon;
+		}
+		
+		myLocationText.setText(latLongString);
 	}
 
 	@Override
