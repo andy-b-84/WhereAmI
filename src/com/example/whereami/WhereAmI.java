4de053c6f72e923ae76e5efaef3bb1726f60ee6b
Patrick Laxton <patrick.laxton@gmail.com>
2013-12-03 10:15:23 +0100

    Chapitre 13 - page 556 - géocodage inverse (in : position, out : address)

--- src/com/example/whereami/WhereAmI.java
+++ src/com/example/whereami/WhereAmI.java
@@ -1,6 +1,12 @@
 package com.example.whereami;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -38,14 +44,58 @@ public class WhereAmI extends Activity {
 		TextView myLocationText = (TextView)findViewById(R.id.myLocationText);
 		
 		String latLongString = "You seem to be nowhere.";
+		String addressString = "No address found.";
 		if (null != location) {
 			double lat = location.getLatitude();
 			double lon = location.getLongitude();
 			
 			latLongString = "Lat : " + lat + "\nLong : " + lon;
+			
+			double latitude = location.getLatitude();
+			double longitude = location.getLongitude();
+			Geocoder gc = new Geocoder(this, Locale.getDefault());
+			try {
+				List<Address> addresses = gc.getFromLocation(latitude, longitude, 1);
+				StringBuilder sb = new StringBuilder();
+				if (addresses.size() > 0) {
+					Address address = addresses.get(0);
+					
+					String addressLine = "";
+					String postalCodeAndLocality = "";
+					String countryName = "";
+					
+					for ( int i = 0; i < address.getMaxAddressLineIndex(); i++ ) {
+						addressLine = address.getAddressLine(i);
+						postalCodeAndLocality = address.getPostalCode() + " " + address.getLocality();
+						countryName = address.getCountryName();
+						
+						if ( ( null != addressLine ) && ( "" != addressLine ) && !addressLine.equals(postalCodeAndLocality) ) {
+							sb.append(addressLine).append("\n");
+							
+							sb.append(postalCodeAndLocality).append(", ");
+							sb.append(countryName).append("\n\n");
+						}
+					}
+					
+					String newAddressString = sb.toString();
+					if ( ( null != newAddressString ) && ( "" != newAddressString ) ) {
+						addressString = newAddressString;
+					} else if ( ( ( null != addressLine ) && ( "" != addressLine ) ) || ( ( null != postalCodeAndLocality ) && ( "" != postalCodeAndLocality ) ) ) {
+						if ( ( null != addressLine ) && ( "" != addressLine ) ) {
+							addressString = addressLine;
+						} else {
+							addressString = postalCodeAndLocality;
+						}
+						
+						if ( ( null != countryName ) && ( "" != countryName ) ) {
+							addressString = addressString + ", " + countryName;
+						}
+					}
+				}
+			} catch ( IOException e ) {}
 		}
 		
-		myLocationText.setText(latLongString);
+		myLocationText.setText("Your current position is :\n\n" + latLongString + "\n\n" + addressString);
 	}
 
 	@Override
